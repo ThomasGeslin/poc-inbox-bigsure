c9294602a16ae0dce3f1fb666460a62df1d71764
BATIBIG\tgeslin <tgeslin@batibig.com>
2026-06-16 17:25:28 +0200

    chore: add enpoint to cors authorization

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   });
   app.setGlobalPrefix('api');
